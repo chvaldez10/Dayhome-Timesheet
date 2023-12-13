65235167f85fd845667d6a56aea73eea87068c51
chvaldez10 <christianvaldez021@gmail.com>
2023-12-13 10:49:43 -0700

    added docs

--- crafts/my_sql_database.py
+++ crafts/my_sql_database.py
@@ -6,7 +6,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class MySQLDatabase:
+    """
+    A Data Access Object (DAO) class for managing interactions with a MySQL database.
+
+    This class abstracts and encapsulates all access to the data source, providing
+    methods to execute and read SQL queries. 
+
+    Methods:
+        __init__: Initializes the database connection.
+        execute_query: Executes a given SQL query, given data.
+        read_query: Executes a read operation and returns fetched results.
+    """
+
     def __init__(self) -> None:
+        # initialize connection
         self.connection = None
 
         try:
@@ -19,7 +32,42 @@ class MySQLDatabase:
         except Error as e:
             print(f"Error connection to MySQL database: {e}")
 
-    def read_query(self, query):
+    def execute_query(self, query, data=None) -> bool:
+        """
+        Executes SQL query.
+
+        Args:
+            query: MySQL command
+            data: data to inser
+
+        Returns:
+            True for successful query. Otherwise, return false.
+        """
+
+        cursor = self.connection.cursor()
+
+        try:
+            if data:
+                cursor.execute(query, data)
+            else:
+                cursor.execute(query)
+            self.connection.commit()
+            return True
+        except Error as e:
+            print(f"Error executing data: {e}")
+            return False
+
+    def read_query(self, query) -> list:
+        """
+        Queries data from database.
+
+        Args:
+            query: MySQL query statement
+
+        Returns:
+            A list of tuples containing all results.
+        """
+
         cursor = self.connection.cursor()
 
         try:
--- crafts/populate.py
+++ crafts/populate.py
@@ -1,14 +1,25 @@
 # import pandas as pd
 from my_sql_database import MySQLDatabase
+import datetime
 
 def insert_data(database, data):
-    pass
+    insert_query = "INSERT INTO DailyLog (DateEntry, LoginTime, SignOutTime, TotalTime, Status, ChildrenID) VALUES (%s, %s, %s, %s, %s, %s)"
+    return database.execute_query(insert_query, data)
 
 def read_data(database):
     read_query = "SELECT * FROM DailyLog"
     return database.read_query(read_query)
 
+###########################################################
+#
+#            Main loop for input_processing.py
+#
+##########################################################
+
 if __name__ == "__main__":
+    test_data = (datetime.date(2023, 12, 9), datetime.timedelta(seconds=29581), datetime.timedelta(seconds=62101), 9.05, 'Attendance Recorded', 'sweetrice001')
+
     database = MySQLDatabase()
+    insert_data(database, test_data)
 
     print(read_data(database))
