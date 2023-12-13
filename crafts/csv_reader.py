e65ae9ec1212a8baf2ff4d99d5c1a946ceb00eb3
chvaldez10 <christianvaldez021@gmail.com>
2023-12-13 13:48:38 -0700

    added parsing for times

--- crafts/csv_reader.py
+++ crafts/csv_reader.py
@@ -20,11 +20,11 @@ class CSV_Reader:
             child_name = row["Child Name"]
             location = self.clean_location_string(row["Location/Room"])
             status = row["Status"]
-            in_time, out_time = self.parse_datetime(row["Times"])
+            in_time, out_time, total_time = self.parse_datetime(row["Times"])
             health_check = row["Health Check"]
 
             print(in_time, type(in_time))
-            print(child_name, location, status, in_time, out_time, health_check)
+            print(child_name, location, status, in_time, out_time, total_time, health_check)
             break
 
     def clean_location_string(self, location:str) -> str:
@@ -38,9 +38,10 @@ class CSV_Reader:
         if found_dates and len(found_dates) == 2:
             in_time = datetime.strptime(found_dates[0], '%I:%M %p').time()
             out_time = datetime.strptime(found_dates[1], '%I:%M %p').time()
-            return in_time, out_time
+            total_time = (datetime.combine(datetime.min, out_time) - datetime.combine(datetime.min, in_time)).seconds / 3600.0
+            return in_time, out_time, total_time
         else:
-            return None, None
+            return None, None, None
 
 my_csv_reader = CSV_Reader()
 my_csv_reader.read_csv(TEST_CSV)
--- crafts/populate.py
+++ crafts/populate.py
@@ -19,12 +19,10 @@ if __name__ == "__main__":
     in_time = "08:00:00"
     in_time_obj = datetime.datetime.strptime(in_time, "%H:%M:%S").time()
 
-    out_time = "08:00:00"
+    out_time = "15:00:00"
     out_time_obj = datetime.datetime.strptime(out_time, "%H:%M:%S").time()
     
     test_data = (datetime.date(2023, 12, 9), in_time_obj, out_time_obj, 9.05, 'Attendance Recorded', 'sweetrice001')
 
     database = MySQLDatabase()
     insert_data(database, test_data)
-
-    print(read_data(database))
