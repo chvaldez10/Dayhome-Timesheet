952dfc23073a87ad3ca0640705cff9c516eb5271
chvaldez10 <christianvaldez021@gmail.com>
2023-12-13 11:49:10 -0700

    deleted test lines

--- crafts/csv_reader.py
+++ crafts/csv_reader.py
@@ -1,11 +1,9 @@
 import pandas as pd
+import re
+import datetime
 
 TEST_CSV = "2023-12-12.csv"
 
 class CSV_Reader:
     def __init__(self) -> None:
         pass
-
-df = pd.read_csv(TEST_CSV)
-print(df.columns)
-print(df.loc[:, "Times"])
