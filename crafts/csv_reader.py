77aedacf54a92d1b7e17b696a68091befc04576e
chvaldez10 <christianvaldez021@gmail.com>
2023-12-18 12:11:01 -0700

    fixed time parsing bug

--- crafts/csv_reader.py
+++ crafts/csv_reader.py
@@ -20,12 +20,10 @@ class CSV_Reader:
             child_name = row["Child Name"]
             location = self.clean_location_string(row["Location/Room"])
             status = row["Status"]
-            in_time, out_time, total_time = self.parse_datetime(row["Times"])
+            in_time, out_time, total_time = self.parse_datetime(str(row["Times"]))
             health_check = row["Health Check"]
 
-            print(in_time, type(in_time))
-            print(child_name, location, status, in_time, out_time, total_time, health_check)
-            break
+            print("child name: ", child_name, "| status: ", status, "| in time: ", in_time, "| out time: ", out_time, "| total time: ", total_time, "| health check: ", health_check)
 
     def clean_location_string(self, location:str) -> str:
         cleaned_location_string = location.split()
@@ -33,15 +31,19 @@ class CSV_Reader:
         return " ".join(cleaned_location_string)
     
     def parse_datetime(self, times: str):
-        found_dates = re.findall(r"(\d{2}:\d{2} [AP]M)", times)
+        if not times:
+            return None, None, 0
         
-        if found_dates and len(found_dates) == 2:
-            in_time = datetime.strptime(found_dates[0], '%I:%M %p').time()
-            out_time = datetime.strptime(found_dates[1], '%I:%M %p').time()
-            total_time = (datetime.combine(datetime.min, out_time) - datetime.combine(datetime.min, in_time)).seconds / 3600.0
-            return in_time, out_time, total_time
-        else:
-            return None, None, None
+        found_dates = re.findall(r"(\d{2}:\d{2} [AP]M)", times)
+
+        if len(found_dates) != 2:
+            return None, None, 0
+
+        in_time = datetime.strptime(found_dates[0], '%I:%M %p').time()
+        out_time = datetime.strptime(found_dates[1], '%I:%M %p').time()
+        total_time = (datetime.combine(datetime.min, out_time) - datetime.combine(datetime.min, in_time)).seconds / 3600.0
+        
+        return in_time, out_time, total_time
 
 my_csv_reader = CSV_Reader()
 my_csv_reader.read_csv(TEST_CSV)
