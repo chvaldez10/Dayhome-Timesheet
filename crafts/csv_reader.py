46931aeb79c2cb47bf58548566c816fd4fd45525
chvaldez10 <christianvaldez021@gmail.com>
2023-12-18 12:44:52 -0700

    added docstring

--- crafts/csv_reader.py
+++ crafts/csv_reader.py
@@ -7,46 +7,59 @@ TEST_CSV = "2023-12-12.csv"
 
 class CSV_Reader:
     def __init__(self) -> None:
+        """Initialize the CSV Reader class."""
         pass
 
-    def read_csv(self, csv_file:str) -> list[tuple]:
-        # read csv as dataframe
-        df = pd.read_csv(csv_file)
-
-        # drop unwanted columns
-        df.drop(columns=["Unnamed: 0", "Provider", "Unnamed: 7", "Unnamed: 8"], inplace=True)
+    def read_csv(self, csv_file: str) -> list[tuple]:
+        """
+        Reads a CSV file and processes its content.
+        Args:
+            csv_file (str): The path to the CSV file.
+        Returns:
+            list[tuple]: A list of tuples containing processed data from the CSV.
+        """
+        try:
+            df = pd.read_csv(csv_file)
+            df.drop(columns=["Unnamed: 0", "Provider", "Unnamed: 7", "Unnamed: 8"], inplace=True)
+        except Exception as e:
+            print(f"Error reading CSV file: {e}")
+            return []
 
         daily_log = []
-
-        # parse dataframe per row
-        for index, row in df.iterrows():
+        for _, row in df.iterrows():
             child_name = row["Child Name"]
             location = self.clean_location_string(row["Location/Room"])
             status = row["Status"]
             in_time, out_time, total_time = self.parse_datetime(str(row["Times"]))
-            health_check = str(row["Health Check"])
+            health_check = str(row["Health Check"]) if pd.notna(row["Health Check"]) else None
 
-            if health_check == "nan":
-                health_check = None
-            print(type(health_check), health_check)
-            temporary_tuple = (child_name, location, status, in_time, out_time, total_time, health_check)
+            daily_log.append((child_name, location, status, in_time, out_time, total_time, health_check))
 
-            daily_log.append(temporary_tuple)
-        
-        for tup in daily_log:
-            print(tup,"\n", "="*50, "\n")
+        return daily_log
 
-    def clean_location_string(self, location:str) -> str:
-        cleaned_location_string = location.split()
-        cleaned_location_string = [word for word in cleaned_location_string if word != "---AGENCY"]
+    def clean_location_string(self, location: str) -> str:
+        """
+        Cleans a location string by removing specific unwanted parts.
+        Args:
+            location (str): The location string to clean.
+        Returns:
+            str: The cleaned location string.
+        """
+        cleaned_location_string = [word for word in location.split() if word != "---AGENCY"]
         return " ".join(cleaned_location_string)
     
-    def parse_datetime(self, times: str):
+    def parse_datetime(self, times: str) -> tuple:
+        """
+        Parses a datetime string and calculates the total time.
+        Args:
+            times (str): The datetime string to parse.
+        Returns:
+            tuple: A tuple containing in_time, out_time, and total_time.
+        """
         if not times:
             return None, None, 0
         
         found_dates = re.findall(r"(\d{2}:\d{2} [AP]M)", times)
-
         if len(found_dates) != 2:
             return None, None, 0
 
@@ -55,6 +68,3 @@ class CSV_Reader:
         total_time = (datetime.combine(datetime.min, out_time) - datetime.combine(datetime.min, in_time)).seconds / 3600.0
         
         return in_time, out_time, total_time
-
-my_csv_reader = CSV_Reader()
-my_csv_reader.read_csv(TEST_CSV)
