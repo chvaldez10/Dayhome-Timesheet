32ba621637b77cfe8eed39a65e0cf7a48e696950
chvaldez10 <christianvaldez021@gmail.com>
2023-12-25 19:05:09 -0700

    bug fix for creating provider log

--- crafts/main.py
+++ crafts/main.py
@@ -1,19 +1,27 @@
 # user defined classes
 from exceptions.sys_arg_error import SysArgError
+from exceptions.date_parsing_error import DateParsingError
 from database.my_sql_database import MySQLDatabase
 from database.my_sql_queries import insert_data, read_data
 from readers.csv_reader import CSV_Reader
+from utilities.date_utilities import parse_for_valid_date
 from utilities.daily_log_utilities import generate_provider_log
 
 # python libraries
+import os
 import pandas as pd
 import sys
+from datetime import datetime
 
 # test data
-TEST_CSV = "./csv/2023-12-12.csv"
+CSV_FOLDER = "./csv"
 
 # column names 
 COLUMN_NAMES_CRAFTS = ["Date Entry", "Child Name", "Location", "Status", "In Time", "Out Time", "Total Time", "Health Record"]
+
+# config to see all columns
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
 
 
 ###########################################################
@@ -22,21 +30,28 @@ COLUMN_NAMES_CRAFTS = ["Date Entry", "Child Name", "Location", "Status", "In Tim
 #
 ##########################################################
 
+
 def main(provider_id: str):
     # Setup database and CSV reader
     # my_database = MySQLDatabase()
     my_csv_reader = CSV_Reader()
 
-    # Read and process the CSV file
-    daily_log = my_csv_reader.read_csv(TEST_CSV)
-    daily_log_df = pd.DataFrame(daily_log, columns=COLUMN_NAMES_CRAFTS)
+    csv_files = sorted(os.listdir(CSV_FOLDER))
 
-    pd.set_option("display.max_columns", None)
-    pd.set_option("display.width", None)
-
-    # Generate and print the provider log
-    provider_log = generate_provider_log(daily_log_df, provider_id)
-    print(provider_log)
+    for csv_file in csv_files:
+        temp_filename = os.path.join(CSV_FOLDER, csv_file)
+        print(csv_file)
+        try:
+            parse_for_valid_date(csv_file)
+        except DateParsingError as e:
+            print(f"{e}\nInvalid filename: {temp_filename}")
+        else:
+            # Read and process the CSV file
+            daily_log = my_csv_reader.read_csv(temp_filename)
+            daily_log_df = pd.DataFrame(daily_log, columns=COLUMN_NAMES_CRAFTS)
+            provider_log = generate_provider_log(daily_log_df, provider_id)
+            print(daily_log_df)
+            print(provider_log)
 
 
 if __name__ == "__main__":
--- crafts/utilities/daily_log_utilities.py
+++ crafts/utilities/daily_log_utilities.py
@@ -2,7 +2,6 @@ import pandas as pd
 from datetime import datetime
 from typing import Tuple
 
-
 def generate_provider_log(daily_log_df: pd.DataFrame, user_id: str) -> Tuple:
     """
     Generate and print a log entry for a provider given a daily log DataFrame.
@@ -18,5 +17,9 @@ def generate_provider_log(daily_log_df: pd.DataFrame, user_id: str) -> Tuple:
     date_entry = daily_log_df.iloc[0]["Date Entry"]
     min_in_time = daily_log_df["In Time"].dropna().min()
     max_out_time = daily_log_df["Out Time"].dropna().max()
+
+    if (pd.isnan(min_in_time)) or pd.isnan(max_out_time):
+        return (date_entry, None, None, None, user_id)
+    
     total_time = (datetime.combine(datetime.min, max_out_time) - datetime.combine(datetime.min, min_in_time)).total_seconds() / 3600
     return (date_entry, min_in_time, max_out_time, total_time, user_id)
