17685355a30141e4a0673a3615e047224cc8a6bf
chvaldez10 <christianvaldez021@gmail.com>
2023-12-29 08:44:33 -0700

    added docs to process file

--- crafts/utilities/process_file_utilities.py
+++ crafts/utilities/process_file_utilities.py
@@ -5,7 +5,7 @@ from utilities.daily_log_utilities import generate_provider_log
 
 # python packages
 import pandas as pd
-from typing import List, Tuple
+from typing import List, Tuple, Optional, Any
 import os
 
 # config to see all columns
@@ -16,25 +16,34 @@ pd.set_option("display.width", None)
 COLUMN_NAMES_CRAFTS = ["Date Entry", "Child Name", "Location", "Status", "In Time", "Out Time", "Total Time", "Health Record"]
 
 
-def process_file(filename: str, reader: CSV_Reader) -> pd.DataFrame:
-    """Process each CSV file: read, convert to DataFrame, and generate logs."""
+def process_file(filename: str, reader: CSV_Reader) -> Optional[pd.DataFrame]:
+    """
+    Process each CSV file: read, convert to DataFrame, and generate logs.
+    :param filename: The name of the file to process.
+    :param reader: CSV Reader instance with a method read_csv.
+    :return: A pandas DataFrame of the processed file or None if an error occurs.
+    """
     try:
         parse_for_valid_date(filename)  # Validate date or filename
         daily_log = reader.read_csv(filename)
         daily_log_df = pd.DataFrame(daily_log, columns=COLUMN_NAMES_CRAFTS)
         print(daily_log_df)
         return daily_log_df
-        provider_log = generate_provider_log(daily_log_df, provider_id)
-        print(provider_log)
     except DateParsingError as e:
         print(f"{e}\nInvalid filename: {filename}")
+        return None
 
 
-def get_provider_log(daily_log_df: pd.DataFrame, provider_id: str) -> Tuple:
+def get_provider_log(daily_log_df: pd.DataFrame, provider_id: str) -> Tuple[Any, ...]:
+    """
+    Extracts logs for a specific provider from the daily log DataFrame.
+    :param daily_log_df: DataFrame containing daily logs.
+    :param provider_id: The provider's identifier to filter logs.
+    :return: A tuple containing the provider's log.
+    """
     provider_log = generate_provider_log(daily_log_df, provider_id)
     print(provider_log)
     return provider_log
-
 
 def get_csv_files(folder_path: str) -> List[str]:
     """Get sorted list of CSV files from the given folder."""
