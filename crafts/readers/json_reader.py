6ffeb3ada1422963bb57f8dfd7093fce1ff9c85e
chvaldez10 <christianvaldez021@gmail.com>
2023-12-29 09:35:41 -0700

    created json reader

---
+++ crafts/readers/json_reader.py
@@ -0,0 +1,16 @@
+import json
+from typing import Dict, Optional
+
+
+def load_json(filename: str) -> Optional[Dict]:
+    """
+    Loads a JSON file and returns its contents as a dictionary.
+    :param filename: The path to the JSON file.
+    :return: A dictionary representation of the JSON file or None if an error occurs.
+    """
+    try:
+        with open("./json/users.json", "r") as users_data:
+            return json.load(users_data)
+    except FileNotFoundError as e:
+        print(f"Error reading {filename}. {e}")
+        return None
