5648e0f411cec871ce1ffb1affe3d6f6d345a17e
chvaldez10 <christianvaldez021@gmail.com>
2024-01-06 11:49:03 -0700

    added html template for email sender

--- crafts/emailer/email_sender.py
+++ crafts/emailer/email_sender.py
@@ -3,43 +3,23 @@ import smtplib
 import ssl
 import imghdr
 from email.message import EmailMessage
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from jinja2 import Template
-
+from jinja2 import FileSystemLoader, Environment
 class EmailSender:
     def __init__(self):
         self.email_sender = os.getenv("EMAIL_SENDER")
         self.email_password = os.getenv("EMAIL_PASSWORD")
         self.email_receiver = os.getenv("EMAIL_RECEIVER")
         self.subject = "Test Email"
-        self.html_template = """
-        <html>
-            <body>
-                <p>Daily summary for [date]</p>
-                <table style="border-collapse: collapse; width: 100%;">
-                    <tr>
-                        <th style="border: 1px solid #ddd; padding: 8px; text-align: left; background-color: #f2f2f2;">User</th>
-                        <th style="border: 1px solid #ddd; padding: 8px; text-align: left; background-color: #f2f2f2;">Sign In</th>
-                        <th style="border: 1px solid #ddd; padding: 8px; text-align: left; background-color: #f2f2f2;">Sign Out</th>
-                        <th style="border: 1px solid #ddd; padding: 8px; text-align: left; background-color: #f2f2f2;">Total Time</th>
-                    </tr>
-                    {% for user, data in user_data.items() %}
-                    <tr>
-                        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">{{ user }}</td>
-                        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">{{ data[0] }}</td>
-                        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">{{ data[1] }} </td>
-                        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">{{ data[2] }} H</td>
-                    </tr>
-                    {% endfor %}
-                </table>
-                <img src="cid:radiance-logo-no-bg">
-            </body>
-        </html>
-        """
 
+        # html template
+        self.template_loader = FileSystemLoader(searchpath="./../frontend/public/")
+        self.template_env = Environment(loader=self.template_loader)
+        self.template_file = "email_template.html"
+
+        # image
         self.image_path = "./../frontend/public/radiance-logo-no-bg.png"
 
+        # user data
         self.user_data = {}
 
     def send_email(self):
@@ -49,11 +29,11 @@ class EmailSender:
         em["Subject"] = self.subject
 
         # render template
-        template = Template(self.html_template)
+        template = self.template_env.get_or_select_template(self.template_file)
         html_content = template.render(user_data=self.user_data)
         em.add_alternative(html_content, subtype="html")
 
-        # Attach an image
+        # attach image
         with open(self.image_path, 'rb') as img:
             image_data = img.read()
             image_type = imghdr.what(img.name)
