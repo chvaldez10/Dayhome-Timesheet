40728f63a78260893c88b07a2a801bfabfd07ddd
chvaldez10 <christianvaldez021@gmail.com>
2024-06-28 17:05:33 -0600

    deleted + moved folders to re purpose project

---
+++ automation/csv-to-mysql/database/populate.sql
@@ -0,0 +1,12 @@
+INSERT INTO Provider (ProviderID, FirstName, LastName, Location) 
+VALUES ("wendy.valdez", "Wendy", "Valdez", "Day Homes: Wendy Valdez");
+
+INSERT INTO Children (ChildrenID, FirstName, LastName, ProviderID) VALUES
+("alanna.ahmed", "Alanna", "Ahmed", "wendy.valdez"),
+("amanda.montes", "Amanda Leigh", "Montes", "wendy.valdez"),
+("amara.montes", "Amara Louise", "Montes", "wendy.valdez"),
+("jaiveer.singh", "Jaiveer", "Singh", "wendy.valdez"),
+("japneet.kaur", "Japneet", "Kaur", "wendy.valdez"),
+("matthew.ubalde", "Matthew Ethan", "Ubalde", "wendy.valdez"),
+("kandace.castelo", "Kandace Alyssa", "Castelo", "wendy.valdez")
+;
--- frontend/README.md
+++
@@ -1,3 +0,0 @@
-# Frontend Project for DayHome App
-
-TBD (Have not started)
